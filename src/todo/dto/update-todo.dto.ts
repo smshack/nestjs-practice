6441d3279379fcdc9a8f16333ba082c055006311
jerry <jerry@wattsolution.co.kr>
2024-12-02 14:38:38 +0900

    유효성 검사 예시 부족분 추가

--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateTodoDto } from './create-todo.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {} 
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {
+  @ApiPropertyOptional({
+    description: '완료 여부',
+    example: true
+  })
+  @IsOptional()
+  @IsBoolean({ message: '완료 여부는 boolean 타입이어야 합니다.' })
+  isCompleted?: boolean;
+} 
